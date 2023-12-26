14f97077516c9a2ab3de3e8a5fc9c35e874a5441
ArtZ <40014815+Sempreyo@users.noreply.github.com>
2023-12-26 03:56:22 +0300

    project init

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// import ready from "../js/utils/documentReady.js";
+
+// ready(function () {
+//   console.log("DOM героически построен!");
+//   // Добавление кастомного свойства с системной шириной скролла
+//   document.documentElement.style.setProperty("--css-scroll-size", `${getScrollSize()}px`);
+// });
+
+// import $ from "jquery"; // Перед использованием установить как зависимость
+// $(function () {
+//   console.log("jQuery героически сработал!");
+// });
