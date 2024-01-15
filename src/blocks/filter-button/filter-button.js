ce0d83dad3daf4327fa8fec75abfb394081da450
a.zhukov <a.zhukov@qualitica.ru>
2024-01-15 17:28:24 +0300

    fix: rename btn to button

--- src/blocks/filter-button/filter-button.js
+++ src/blocks/filter-button/filter-button.js
@@ -2,11 +2,11 @@ import ready from "../../js/utils/documentReady.js";
 import toggleState from "../../js/common/toggleState.js";
 
 ready(function () {
-  const filterBtn = document.querySelector(".filter-btn");
+  const filterButton = document.querySelector(".filter-button");
   const overlay = document.querySelector(".page__overlay");
 
-  if (filterBtn) {
-    filterBtn.addEventListener("click", () => {
+  if (filterButton) {
+    filterButton.addEventListener("click", () => {
       toggleState(document.body, "mobile-menu");
     });
 
