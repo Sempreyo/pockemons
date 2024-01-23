ad9f2801c983b6ec8c59f2336a8c54e2629b619b
ArtZ <40014815+Sempreyo@users.noreply.github.com>
2024-01-23 10:52:55 +0300

    fix: pagination

--- src/js/common/getPaginationState.js
+++ src/js/common/getPaginationState.js
@@ -1,32 +1,16 @@
-function getPaginationState(current, total, length, firstButton, lastButton) {
+function getPaginationState(current, total, length) {
   let start = Math.min(current - Math.floor(length / 2), total + 1 - length);
-  const startFiller = firstButton.nextElementSibling;
-  const lastFiller = lastButton.previousElementSibling;
+  let values = [];
 
   if (length > total) length = total;
 
   for (let i = 0; i < length; i++) {
     const value = (start < 1 ? 1 : start) + i;
-    const paginationNum = `<button class="pagination__link">${value}</button>`;
 
-    lastFiller.insertAdjacentHTML("beforebegin", paginationNum);
-
-    if (startFiller.nextElementSibling.textContent == 1) {
-      firstButton.setAttribute("hidden", true);
-      startFiller.setAttribute("hidden", true);
-    } else {
-      firstButton.removeAttribute("hidden");
-      startFiller.removeAttribute("hidden");
-    }
-
-    if (lastFiller.previousElementSibling.textContent == total) {
-      lastButton.setAttribute("hidden", true);
-      lastFiller.setAttribute("hidden", true);
-    } else {
-      lastButton.removeAttribute("hidden");
-      lastFiller.removeAttribute("hidden");
-    }
+    values.push(value);
   }
+
+  return values;
 }
 
 export default getPaginationState;
